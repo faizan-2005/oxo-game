adc224ca9a35fc0f357ff252f9f91f701ba067ba
Faizan Baig <Faizanbaig.com@gmail.com>
2026-09-10 00:50:02 +0530

    docs: add build for live

---
+++ docs/_next/static/yQJlgyZ7UN-vZr5v1m0SB/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/oxo-game//_next/:path+",
+        "destination": "/oxo-game/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
